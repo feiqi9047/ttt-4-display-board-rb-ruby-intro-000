1a3515344d6a04dfb06eabc3d087197619ba7099
Fei Qi <feiqi9047@gmail.com>
2018-11-26 23:21:16 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -150,16 +150,6 @@ describe "#display_board in 'lib/display_board.rb" do
       
     end 
     
-      # Should you want to write your own test for this situation,
-      # read the following code and comments.
-
-      # Can you copy the syntax of the tests above to write a test for a board
-      # entirely filled with Xs?"
-
-      # Define the board with values that should create the desired output
-      # *** Edit the line below ***
-      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
-
       # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
       rows = output.split("\n")
@@ -185,10 +175,21 @@ describe "#display_board in 'lib/display_board.rb" do
       # expect(rows[4]).to eq("   |   |   ")
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+     # expect(true).to be(true)
     end
 
     it 'prints an entire board full of Os' do
+         board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq(" O | O | O ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" O | O | O ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" O | O | O ")
+    end 
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
 
@@ -197,7 +198,7 @@ describe "#display_board in 'lib/display_board.rb" do
       # example's situation.
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
     end
   end
 end
